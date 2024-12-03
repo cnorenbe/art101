7c8bed1354681e8d8dc5d77d1485006b5e4eaeef
cnorenbe <cnorenbe@ucsc.edu>
2024-12-02 23:32:45 -0800

    fixes

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -6,32 +6,24 @@
 
 // Using the core $.ajax() method
 $.ajax({
-    // The URL for the request (XKCD comic endpoint)
-    url: "https://xkcd.com/info.0.json",
-    // Type of request (GET request)
-    type: "GET",
-    // Expected data type from the response
-    dataType: "json",
-    // What to do when the request is successful
+    url: "https://xkcd.com/info.0.json",  // URL of the XKCD comic API
+    type: "GET",  // HTTP GET method
+    dataType: "jsonp",  // Using JSONP to avoid CORS issues
     success: function(comicObj) {
-        // Output the title of the comic
+        // Extract comic details
         var comicTitle = comicObj.title;
-
-        // Output the comic image
         var comicImg = comicObj.img;
-        var comicAlt = comicObj.alt;  // Alt text for the image
+        var comicAlt = comicObj.alt;
 
-        // Create an HTML string for the comic
+        // Generate HTML content for comic
         var comicHTML = '<h3>' + comicTitle + '</h3>' +
                         '<img src="' + comicImg + '" alt="' + comicAlt + '" title="' + comicAlt + '">' +
                         '<p>' + comicAlt + '</p>';
 
-        // Insert the comic HTML into the #comic-container div
+        // Display the comic in the container
         $('#comic-container').html(comicHTML);
     },
-    // What to do if the request fails
     error: function(jqXHR, textStatus, errorThrown) {
-        // Log any errors to the console
         console.log("Error:", textStatus, errorThrown);
     }
 });
